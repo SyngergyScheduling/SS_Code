0df8eeb9842e19144e76705198f8741eedcc608b
doxterpepper <dockconnellyoneal@gmail.com>
2016-10-25 13:12:29 -0400

    Added a referee modele with a username and password. Password is a digest type so it is compatable with the bcrypt gem. The logic to log a user in is now there but cannot be tested until users can be created.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,21 @@
 class LoginController < ApplicationController
   def login
+    @fail ||= false
   end
 
   def submit
+    username = params['login']['username']
+    password = params['login']['password']
+    ref = Referee.find_by_username(username)
+    if ref && ref.authenticate(password)
+      session[:user_id] = ref.id
+      redirect_to '/'
+    else
+      redirect_to '/login/login'
+    end
+  end
+
+  def create
+    
   end
 end
---
+++ app/models/referee.rb
@@ -0,0 +1,3 @@
+class Referee < ApplicationRecord
+  has_secure_password
+end
