1578f2cf7c65517490f649d8c0bfe7b5d95ead48
doxterpepper <dockconnellyoneal@gmail.com>
2016-10-25 20:32:05 -0400

    Added todo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  # This is temporary for right now.
+  # TODO: change null_session to :exception if you
+  # get the auth tokens working.
   protect_from_forgery with: :null_session
 end
