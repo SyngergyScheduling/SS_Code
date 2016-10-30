844bc974cf410c50cb557d9f87971e1d03c3cc30
Deathande <dockconnellyoneal@gmail.com>
2016-10-30 15:57:16 -0400

    Added the team captain as an optional column for teams

--- db/migrate/20161026011513_create_teams.rb
+++ db/migrate/20161026011513_create_teams.rb
@@ -2,7 +2,7 @@ class CreateTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :teams do |t|
       t.text :name, limit: 32, null: false
-
+      t.text :captain, limit: 32
       t.timestamps
     end
     add_index :teams, :name, unique: true
