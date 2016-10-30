a376f0f455389dd339d612624d9600da2b017886
Deathande <dockconnellyoneal@gmail.com>
2016-10-30 15:53:50 -0400

    Added a test case for the team model unit test

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
-
    test "should not save team without name" do
      team = Team.new
      assert_not team.save
+   end
+
+   test "create team" do
+     team = Team.new({'name': 'Defenders'})
+     assert team.save
    end
 end
