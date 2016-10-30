accb521015e1cf7a6ec453248f7489ce4b06ff34
Deathande <dockconnellyoneal@gmail.com>
2016-10-30 16:55:26 -0400

    Added unit tests for loggin in.

--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class LoginControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "login as a referee" do 
+    login_params = {
+      'login': {
+        'username': 'MyText',
+        'password': 'secret'
+      }
+    }
+    post '/login/submit', params: login_params
+    # When logged in the user will be redirected
+    # to the home page
+    assert_redirected_to '/', 'could not login'
+  end
+
+  test "fail login as a referee with wrong username" do
+    login_params = {
+      'login': {
+        'username': 'Batman',
+        'password': 'secret'
+      }
+    }
+
+    post '/login/submit', params: login_params
+
+    # When login fails the user is redirected to
+    # the login page
+    assert_redirected_to '/login', 'should not have logged in'
+  end
 end
