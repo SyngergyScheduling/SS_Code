71922b8656aeabb3d8c2caaa2b9942f1d0be7a4d
doxterpepper <dockconnellyoneal@gmail.com>
2016-11-01 17:29:16 -0400

    Added negative tests for referee as well as added validations to referee model. The referee model also defaults to level 0.

--- app/models/referee.rb
+++ app/models/referee.rb
@@ -1,3 +1,10 @@
 class Referee < ApplicationRecord
+  validates :username, presence: true
+  validates :password, presence: true
   has_secure_password
+  after_initialize :init
+
+  def init
+    self.level ||= 0
+  end
 end
