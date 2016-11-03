e0781bdda1c6cce62852c9fd234ffdd44e0a734f
doxterpepper <dockconnellyoneal@gmail.com>
2016-11-03 03:53:00 -0400

    Algorithm has been moved to the controller for generating the schedule. The schedule can be generated now it needs to be stored in the DB. Once that is done we just need to display it all.

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -16,15 +16,26 @@ class LeagueController < ApplicationController
         render "Error!"
       end
       unless value.eql? ""
-        teams << Team.new({'name': value})
-	team_ids << teams[-1].id
+        teams << Team.new('name': value)
         begin
 	  teams[-1].save
+	  team_ids << teams[-1].id
 	rescue  ActiveRecord::RecordNotUnique
 	  session['error'] << "Team #{value} already exists"
 	  error = true
 	end
       end
+    end
+    league_schedule = schedule(team_ids)
+    # debug printing
+    league_schedule.each do |item|
+      print("[")
+      item.each do |sid|
+        print(" #{sid} ")
+      end
+      puts "]"
+    end
+    league_schedule.each do |day|
     end
     if error
       redirect_to league_create_url
@@ -33,4 +44,23 @@ class LeagueController < ApplicationController
     session['error'] = nil
     redirect_to '/'
   end
+  
+  def schedule(teams)
+    days = []
+    teams << nil if teams.size % 2 != 0
+    (teams.size-1).times do
+      day = []
+      # When there are an odd number of items
+      # we add one so the size is guaranteed to
+      # be divisible by 2 by the definition of
+      # even numbers.
+      (teams.size / 2).times do |i|
+        day << [teams[i], teams[-(i+1)]]
+      end
+      days << day
+      teams.insert(0, teams.delete_at(teams.size-2))
+    end
+    return days
+  end
+
 end
--- app/helpers/league_helper.rb
+++ app/helpers/league_helper.rb
@@ -1,21 +1,2 @@
 module LeagueHelper
-  def schedule(teams)
-    days = []
-    teams << nil if teams.size % 2 != 0
-    teams.size.times do
-      day = []
-      # When there are an odd number of items
-      # we add one so the size is guaranteed to
-      # be divisible by 2 by the definition of
-      # even numbers.
-      (teams.size / 2).times do |i|
-        day << [teams[i], teams[-(i+1)]]
-      end
-      days << day
-      tmp = teams[0]
-      teams[0] = teams[-2]
-      teams[-2] = tmp
-    end
-    return days
-  end
 end
