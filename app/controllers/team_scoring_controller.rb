3a7aee9ab0679a2d34524e8b1c4b0cf393e9f25b
Olyn Dabbs <ohdabbs@gmail.com>
2016-11-13 18:36:37 -0500

    Made scoring update the team1_score and team2_score attributes in the schedule table rather than the score attribute in the team table.  This will allow rescoring for particular matches.

--- app/controllers/team_scoring_controller.rb
+++ app/controllers/team_scoring_controller.rb
@@ -7,13 +7,16 @@ class TeamScoringController < ApplicationController
     end
     cookies[:team1] = params['id1']
     cookies[:team2] = params['id2']
+	cookies[:match] = params['match']
     @team1 = Team.find_by(id: params['id1'])
     @team2 = Team.find_by(id: params['id2'])
+	@match = Schedule.find_by(id: params['match'])
   end
 
   def score
     @team1 = Team.find_by(id: cookies[:team1])
     @team2 = Team.find_by(id: cookies[:team2])
+	@match = Schedule.find_by(id: cookies[:match])
     fields = ['Style', 'Presentation', 'Understanding', 'Response']
     team1 = 0
     team2 = 0
@@ -21,10 +24,9 @@ class TeamScoringController < ApplicationController
       team1 += params['scoring']["Team1-#{field}"].to_i
       team2 += params['scoring']["Team2-#{field}"].to_i
     end
-    @team1.score += team1
-    @team2.score += team2
-    @team1.save
-    @team2.save
+    @match.update(team1_score: team1)
+    @match.update(team2_score: team2)
+    @match.save
     redirect_to teams_all_url
   end
 end
