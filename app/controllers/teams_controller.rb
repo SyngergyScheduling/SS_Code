5ca1fccb3cda0e88536d12d2dc27b4a4817bd20c
doxterpepper <dockconnellyoneal@gmail.com>
2016-12-01 19:01:24 -0500

    Adding an api and script to clear the database on the server.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,6 +9,15 @@ class TeamsController < ApplicationController
     render :json => {'teams_id': team.id}
   end
 
+  def clear
+    Team.all.each do |team|
+      team.delete
+    end
+    Schedule.all.each do |sched|
+      sched.delete
+    end
+  end
+
   private
   def team_params
     params.permit(:name)
